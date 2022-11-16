d272885513bbe704a6023d6b283eab230604d01d
Adin Ashby <adin.ashby@gmail.com>
2022-11-16 15:54:24 -0500

    Uploaded starter BMI App

---
+++ Lectures/starter_bmi_app_flutter-main/lib/IconContent.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const labelTextStyle = TextStyle(
+    fontSize: 18.0,
+    color: Color(0xFF8D8E98)
+);
+
+class IconContent extends StatelessWidget {
+
+  IconContent({required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: labelTextStyle,
+        )
+      ],
+    );
+  }
+}
