c875a5bd14c3dd54298b0b673d23e76c4702f457
Adin Ashby <adin.ashby@gmail.com>
2022-11-23 16:03:28 -0500

    Added Clima app.

---
+++ Lectures/clima_app_flutter/lib/services/weather.dart
@@ -0,0 +1,61 @@
+import 'location.dart';
+import 'networking.dart';
+
+class WeatherModel {
+
+  final String apiKey = '797e4d8ebe0f188d30df398159ffb418';
+  final String openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper
+          = NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper
+          = NetworkHelper('$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
